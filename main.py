93c48ecb4e0692436aca0726e677be3ec22cf013
Dip3102001 <dippatel3102001@gmail.com>
2024-07-26 03:42:59 +0000

    updated main.py

---
+++ main.py
@@ -0,0 +1,91 @@
+import logging;
+logging.disable(logging.WARNING);
+
+import os;
+
+# downloading weights
+os.system("mkdir -p ~/.kaggle");
+os.system("mv kaggle.json ~/.kaggle/");
+os.system("chmod 600 ~/.kaggle/kaggle.json");
+os.system("kaggle kernels output dippatel03/bartsum-topk-training-t -p /content/Clickbait-2/weights/");
+
+
+
+from transformers import BartTokenizer, BartForConditionalGeneration;
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer;
+
+import torch;
+import torch.nn as nn;
+import torch.optim as optim;
+import torch.nn.functional as F;
+from torch.utils.data import Dataset, DataLoader;
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu");
+print(device);
+
+model = BartForConditionalGeneration.from_pretrained('/content/Clickbait-2/weights/checkpoint-500');
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-base');
+
+import numpy as np;
+import pandas as pd;
+
+
+class TestDataset(nn.Module):
+    def __init__(self,path_x,path_tags,tokenizer,max_seq_length,max_target_length):
+        super().__init__();
+
+        self.max_seq_length = max_seq_length;
+        self.max_target_length = max_target_length;
+
+        with open(path_x,'r') as f:
+            self.x = f.readlines();
+
+        self.df = pd.read_csv(path_tags);
+
+    def __len__(self):
+        return len(self.x);
+
+    def __getitem__(self,idx):
+        x = tokenizer(self.df['spoilerType'][idx],self.x[idx],max_length=self.max_seq_length,truncation=True,padding='max_length',return_tensors='pt');
+        
+        return {
+            'input_ids':x['input_ids'].flatten(),
+            'attention_mask':x['attention_mask'].flatten(),
+        };
+
+
+test_dataset = TestDataset(
+    path_x='/kaggle/working/testset_cmb_T_topK.txt',
+    path_tags = "/kaggle/working/roberta_BERTSUM_SEQ_submission_T_5.csv",
+    tokenizer=tokenizer,
+    max_seq_length=512,
+    max_target_length=512, 
+);
+
+test_dataloader = DataLoader(test_dataset,batch_size=8);
+
+
+buffer = [];
+model.eval()
+for batch in test_dataloader:
+    input_ids = batch['input_ids'].to(device);
+    attention_mask = batch['attention_mask'].to(device);
+    
+    output = model.generate(input_ids,attention_mask=attention_mask,max_length=300,num_beams=5,early_stopping=True);
+    txt = tokenizer.batch_decode(output,skip_special_tokens=True);
+    buffer.append(txt);
+
+
+tmp = [];
+for batch in buffer:
+    for line in batch:
+        tmp.append(line.rstrip());
+
+
+data = {
+    "id" : np.arange(400),
+    "spoiler" : tmp
+}; 
+df = pd.DataFrame(data);   
+
+df.to_csv("submission_topK_T.csv",index=False);
